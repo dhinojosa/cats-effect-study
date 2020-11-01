ec437496d28c2508ba5c460819b44dd4612fe2f0
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2020-11-01 13:47:32 -0700

    added sync spec

--- src/test/scala/com/xyzcorp/typeclasses/SyncSpec.scala
+++ src/test/scala/com/xyzcorp/typeclasses/SyncSpec.scala
@@ -1,4 +1,12 @@
 package com.xyzcorp.typeclasses
-class SyncSpec {
 
+import cats.effect.{Clock, IO, Sync}
+import org.scalatest.{FunSpec, Matchers}
+
+class SyncSpec extends FunSpec with Matchers {
+  describe("""A Monad that can suspend the execution of
+             |  side effects in the F[_] context.""".stripMargin) {
+    val f = Sync[IO]
+    val a: IO[Int] = f.delay(14 + 10)
+  }
 }
